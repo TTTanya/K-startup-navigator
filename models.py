350f07e98d33c9e339e504fa88568ac5bd91cd5c
TTTanya <32129133+TTTanya@users.noreply.github.com>
2021-04-13 21:04:14 +0300

    Update models.py

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Contact(models.Model):
+    email = models.EmailField(max_length=200)
     message = models.TextField(max_length=1000)
 
     def __str__(self):
